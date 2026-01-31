14b3ce9a55eea2206d162dc90346f280fe554fe6
IP80808080 <kasambe.kamlesh1@gmail.com>
2026-01-31 14:27:23 +0530

    Phase-Log/Logger Added

---
+++ Logger/LoggerSol/Logger/Controllers/LogController.cs
@@ -0,0 +1,31 @@
+﻿using Logger.Models;
+using Logger.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Logger.Controllers
+{
+    [ApiController]
+    [Route("/logs")]
+    public class LogController : ControllerBase
+    {
+        private readonly FileLoggerService _fileLogger;
+
+        public LogController()
+        {
+            _fileLogger = new FileLoggerService();
+        }
+
+        [HttpPost]
+        public IActionResult CreateLog([FromBody] LogRequest request)
+        {
+            if (request == null)
+            {
+                return BadRequest("Log request is null");
+            }
+
+            _fileLogger.WriteLog(request);
+
+            return Ok(new { message = "Log saved successfully" });
+        }
+    }
+}
---
+++ Logger/LoggerSol/Logger/Services/FileLoggerService.cs
@@ -0,0 +1,39 @@
+﻿using Logger.Models;
+
+namespace Logger.Services
+{
+    public class FileLoggerService
+    {
+        private readonly string _logFilePath;
+        private static readonly object _lockObj = new object(); 
+
+        public FileLoggerService()
+        {
+            var logDirectory = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                "Logs"
+            );
+
+            Directory.CreateDirectory(logDirectory); 
+
+            _logFilePath = Path.Combine(
+                logDirectory,
+                "application-log.txt"
+            );
+        }
+
+        public void WriteLog(LogRequest log)
+        {
+            var logLine =
+                $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} " +
+                $"[{log.Level}] " +
+                $"[{log.Source}] " +
+                $"{log.Message}";
+
+            lock (_lockObj)
+            {
+                File.AppendAllText(_logFilePath, logLine + Environment.NewLine);
+            }
+        }
+    }
+}
