b12fac7449098c8b090faf417d441517c95a37ef
IP80808080 <kasambe.kamlesh1@gmail.com>
2026-02-03 00:57:35 +0530

    Added/Logger

---
+++ Logger/LoggerSol/Logger/Models/LogRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Logger.Models
+{
+    public class LogRequest
+    {
+        public string Level { get; set; }    // INFO, ERROR, DEBUG
+        public string Message { get; set; }  // Actual log text
+        public string Source { get; set; }   // SpringBoot-Backend
+    }
+}
