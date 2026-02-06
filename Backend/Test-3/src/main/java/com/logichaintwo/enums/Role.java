424a76678d0a87b334a4946bd31dfc2ab97a1aed
IP80808080 <kasambe.kamlesh1@gmail.com>
2026-02-06 23:24:04 +0530

    Remove Backend/Test-3 folder

--- Backend/Test-3/src/main/java/com/logichaintwo/enums/Role.java
+++
@@ -1,9 +0,0 @@
-package com.logichaintwo.enums;
-
-public enum Role {
-    ADMIN,
-    WAREHOUSE_MANAGER,
-    CUSTOMER_SUPPORT,
-    CUSTOMER,
-    PRODUCT_MANAGER
-}
--- Backend/Test-3/src/main/java/com/logichaintwo/repository/InventoryTransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.logichaintwo.repository;
-
-import com.logichaintwo.entities.InventoryTransaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface InventoryTransactionRepository extends JpaRepository<InventoryTransaction, Long> {
-    List<InventoryTransaction> findByInventoryId(Long inventoryId);
-}
